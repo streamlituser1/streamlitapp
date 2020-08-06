810085a4344f3421c1e24e7ccada5621b03769dc
Streamlit User 1 <62953650+streamlituser1@users.noreply.github.com>
2020-08-06 13:13:42 -0600

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import numpy as np
+
+st.write("Hello World!")
+
+first_name = st.text_input('First name')
+if st.checkbox('Add last name'):
+    last_name = st.text_input('Last name')
+else:
+    last_name = ''
+repetitions = st.slider('Repetitions')
+
+st.write(' '.join([first_name, last_name] * repetitions))
